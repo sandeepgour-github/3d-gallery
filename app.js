ef80140c1d4e4817b7b918bbb9a2f4ab4b128eb5
sandeepgour-git <sandeepgour597@gmail.com>
2024-06-29 17:30:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const imgEle = document.querySelector(".image-container");
+
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const img = document.querySelector("img");
+console.log(img);
+
+let x = 0;
+let timer;
+
+function updateGallery() {
+  imgEle.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+  timer = setTimeout(() => {
+    x = x - 45;
+    updateGallery();
+  }, 2000);
+}
+
+updateGallery();
+
+prev.addEventListener("click", () => {
+  x = x + 45;
+  clearTimeout(timer);
+  updateGallery();
+});
+
+next.addEventListener("click", () => {
+  x = x - 45;
+  clearTimeout(timer);
+  updateGallery();
+});
+
+img.addEventListener("click", () => {
+  console.log("image clicked");
+  if (img.style.animationPlayState === "paused") {
+    img.style.animationPlayState = "running";
+  }
+});
